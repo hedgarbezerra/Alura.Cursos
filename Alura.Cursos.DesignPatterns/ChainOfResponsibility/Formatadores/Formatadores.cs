f9561ae74c0729c2f4d60d274a242077fe6e36b4
Hedgar Bezerra <hedgarbezerra35@outlook.com>
2022-04-13 21:35:29 -0300

    Mais exemplos finais de Chain of responsibility

---
+++ Alura.Cursos.DesignPatterns/ChainOfResponsibility/Formatadores/Formatadores.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace Alura.Cursos.DesignPatterns.ChainOfResponsibility.Formatadores
+{
+    enum Formato
+    {
+        XML,
+        CSV,
+        PORCENTO
+    }
+
+    class Requisicao
+    {
+        public Formato Formato { get; private set; }
+
+        public Requisicao(Formato formato)
+        {
+            this.Formato = formato;
+        }
+    }
+    class Conta
+    {
+        public Conta()
+        {
+        }
+
+        public Conta(string titular, double saldo)
+        {
+            Titular = titular;
+            Saldo = saldo;
+        }
+
+        public string Titular { get; set; }
+        public double Saldo { get; set; }
+    }
+    interface ICadeiaResposta
+    {
+        string Lidar(Requisicao req, Conta conta);
+        void DefinirProximo(ICadeiaResposta proximo);
+    }
+    abstract class CadeiaRespostaBase : ICadeiaResposta
+    {
+        protected ICadeiaResposta Proximo { get; private set; }
+        public void DefinirProximo(ICadeiaResposta proximo) => this.Proximo = proximo;
+        public virtual string Lidar(Requisicao req, Conta conta)
+        {
+            if (TipoDesejado(req)) return ContaFormatada(conta);
+            else
+                return Proximo is null ? string.Empty : Proximo.Lidar(req, conta);
+        }
+        public abstract string ContaFormatada(Conta conta);
+        public abstract bool TipoDesejado(Requisicao req);
+    }
+
+    class FormatadorRequisicaoEncadeado
+    {
+        private readonly List<ICadeiaResposta> _formatadores;
+        public FormatadorRequisicaoEncadeado(params ICadeiaResposta[] formatadores)
+        {
+            _formatadores = formatadores?.ToList() ?? throw new ArgumentNullException(nameof(formatadores));
+        }
+        public string Formatar(Requisicao req, Conta conta)
+        {
+            if (_formatadores.Count > 1)
+            {
+                for (var handlerIndex = 0; handlerIndex < _formatadores.Count - 1; handlerIndex++)
+                {
+                    _formatadores[handlerIndex].DefinirProximo(_formatadores[handlerIndex + 1]);
+                }
+            }
+            return _formatadores[0].Lidar(req, conta);
+        }
+    }
+
+    class FormatadorRequisicaoXML : CadeiaRespostaBase
+    {
+        public override string ContaFormatada(Conta conta)
+        {
+            var raiz = new XElement("conta");
+            conta.GetType()
+                .GetProperties()
+                .ToList()
+                .ForEach(p => raiz.SetAttributeValue(p.Name.ToLower(), p.GetValue(conta).ToString()));
+
+            return raiz.ToString();
+        }
+
+        public override bool TipoDesejado(Requisicao req) => req.Formato == Formato.XML;
+    }
+    class FormatadorRequisicaoCSV : CadeiaRespostaBase
+    {
+        public override string ContaFormatada(Conta conta) => string.Join(",", conta.GetType()
+            .GetProperties()
+            .Select(p => p.GetValue(conta).ToString()));
+
+        public override bool TipoDesejado(Requisicao req) => req.Formato == Formato.CSV;
+    }
+    class FormatadorRequisicaoPorcentagem : CadeiaRespostaBase
+    {
+        public override string ContaFormatada(Conta conta) => String.Join("%", conta.GetType()
+            .GetProperties()
+            .Select(p => p.GetValue(conta).ToString()));
+
+        public override bool TipoDesejado(Requisicao req) => req.Formato == Formato.PORCENTO;
+    }
+}
--- Alura.Cursos.DesignPatterns/Program.cs
+++ Alura.Cursos.DesignPatterns/Program.cs
@@ -1,8 +1,7 @@
-﻿using Alura.Cursos.DesignPatterns.ChainOfResponsibility;
-using Alura.Cursos.DesignPatterns.ChainOfResponsibility.Descontos;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Xml.Linq;
 
 namespace Alura.Cursos.DesignPatterns
 {
@@ -10,32 +9,7 @@ namespace Alura.Cursos.DesignPatterns
     {
         static void Main(string[] args)
         {
-            var descontoItens = new DescontoMaisCincoItens();
-            var descontoValor = new DescontoValorMaior500();
-            var descontoVendaCasada = new DescontoVendaCasada();
-
-            var calculadorDesconto = new CalculadorDesconto();
-
-            var orcamento = new Orcamento()
-            {
-                Itens = new List<Item>()
-                {
-                    new Item("PC", 100),
-                    new Item("lapis", 10),
-                    new Item("caneta", 10),
-                    new Item("Xbox", 100)
-                }
-            };
-
-            var desconto1 = calculadorDesconto.CalcularDesconto(orcamento);
-            var desconto2 = calculadorDesconto.CalcularDesconto(orcamento, descontoItens);
-            var desconto3 = calculadorDesconto.CalcularDesconto(orcamento, descontoItens, descontoValor, descontoVendaCasada);
-
-            Console.WriteLine(desconto1);
-            Console.WriteLine(desconto2);
-            Console.WriteLine(desconto3);
             Console.ReadLine();
         }
     }
-
 }
