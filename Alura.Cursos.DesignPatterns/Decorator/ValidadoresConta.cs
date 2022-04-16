1ed9905e57a7a7402e70fa3bca4b68a24ee395fb
Hedgar Bezerra <hedgarbezerra35@outlook.com>
2022-04-16 16:49:40 -0300

    Decorator com utilização de strategy e também de template method

---
+++ Alura.Cursos.DesignPatterns/Decorator/ValidadoresConta.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Alura.Cursos.DesignPatterns.Decorator
+{
+    abstract class ValidadorConta
+    {
+        public ValidadorConta ProximoValidador { get; set; }
+        protected abstract string Mensagem { get; }
+        protected ValidadorConta(ValidadorConta proximoValidador = null)
+        {
+            ProximoValidador = proximoValidador;
+        }
+
+        public IEnumerable<string> Validar(Conta conta)
+        {
+            var mensagens = new List<string>();
+
+            if(!ValidarConta(conta))
+                mensagens.Add(Mensagem);
+
+            return ProximoValidador is null ? mensagens : mensagens.Concat(ProximoValidador.Validar(conta));
+        }
+        protected abstract bool ValidarConta(Conta conta);   
+    }
+    class ValidadorTitularConta : ValidadorConta
+    {
+        public ValidadorTitularConta(ValidadorConta proximoValidador = null) : base(proximoValidador) { }
+        protected override string Mensagem => "Titular inválido ou vazio.";
+
+        protected override bool ValidarConta(Conta conta) => !string.IsNullOrEmpty(conta.Titular);
+
+    }
+    class ValidadorAgenciaConta : ValidadorConta
+    {
+        public ValidadorAgenciaConta(ValidadorConta proximoValidador = null) : base(proximoValidador) { }
+        protected override string Mensagem => "Agencia da conta é inválida.";
+
+        protected override bool ValidarConta(Conta conta) => !string.IsNullOrEmpty(conta.Agencia);
+    }
+    class ValidadorDataCriacaoConta : ValidadorConta
+    {
+        public ValidadorDataCriacaoConta(ValidadorConta proximoValidador = null) : base(proximoValidador) { }
+        protected override string Mensagem => "A data de criação da conta é inválida.";
+
+        protected override bool ValidarConta(Conta conta) => DateTime.Today.Date > conta.DataCriacao.Date;
+    }
+    class ValidadorNumeroConta : ValidadorConta
+    {
+        public ValidadorNumeroConta(ValidadorConta proximoValidador = null) : base(proximoValidador) { }
+        protected override string Mensagem => $"O número da conta da conta é inválida.";
+
+        protected override bool ValidarConta(Conta conta) => conta.Numero > 0;
+    }
+    class ValidadoresConta
+    {
+        private readonly ValidadorConta _validadorTitular;
+        private readonly ValidadorConta _validadorAgencia;
+        private readonly ValidadorConta _validadorData;
+        private readonly ValidadorConta _validadorNumero;
+        public ValidadoresConta()
+        {
+            _validadorNumero = new ValidadorNumeroConta();
+            _validadorData = new ValidadorDataCriacaoConta(_validadorNumero);
+            _validadorAgencia = new ValidadorAgenciaConta(_validadorData);
+            _validadorTitular = new ValidadorTitularConta(_validadorAgencia);
+        }
+
+        public IEnumerable<string> Validar(Conta conta) => _validadorTitular.Validar(conta);
+    }
+
+}
--- Alura.Cursos.DesignPatterns/Program.cs
+++ Alura.Cursos.DesignPatterns/Program.cs
@@ -10,22 +10,11 @@ namespace Alura.Cursos.DesignPatterns
     {
         static void Main(string[] args)
         {
-            var contas = new List<Conta>()
-            {
-                new Conta("Fulano1", 300, string.Empty, 1, DateTime.Now.AddMonths(-3)),
-                new Conta("Fulano2", 300, string.Empty, 2, DateTime.Now),
-                new Conta("Fulano3", 900, string.Empty, 3, DateTime.Now),
-                new Conta("Fulano4", 300, string.Empty, 4, DateTime.Now),
-                new Conta("Fulano5", 100, string.Empty, 5, DateTime.Now)
-            };
+            var conta = new Conta("", 300, string.Empty, 1, DateTime.Now.AddMonths(3));
+            var validador = new ValidadoresConta();
 
-            var filtrar = new FiltragemContasFlagadas();
-            var contasFlagadas = filtrar.ContasFlagadas(contas);
-            foreach (var conta in contasFlagadas)
-            {
-                Console.WriteLine(conta.ToString());
-            }   
-
+            var result = validador.Validar(conta);
+            Console.WriteLine(string.Join("\n ", result));
             Console.ReadKey();
         }
     }
