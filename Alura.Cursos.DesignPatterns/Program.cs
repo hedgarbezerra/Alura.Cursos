fe89fe8e71211e62a4fd627e0ed9c696f8b289c2
Hedgar Bezerra <hedgarbezerra35@outlook.com>
2022-07-28 08:06:52 -0300

    Prototype

--- Alura.Cursos.DesignPatterns/Program.cs
+++ Alura.Cursos.DesignPatterns/Program.cs
@@ -1,27 +1,26 @@
-﻿using Alura.Cursos.DesignPatterns.Bridge;
-using Alura.Cursos.DesignPatterns.Command;
+﻿using Alura.Cursos.DesignPatterns.Adapter;
+using Alura.Cursos.DesignPatterns.Prototype;
+using System;
 
 namespace Alura.Cursos.DesignPatterns
 {
     internal class Program
     {
         /// <summary>
-        /// O pattern Command visa ter um comando único para ser utilizado no código todo, por exemplo: Capturar dados de uma NF.
-        /// E esse comand odeve ser executado seguindo um fluxo de trabalho
+        ///
         /// </summary>
         /// <param name="args"></param>
         static void Main(string[] args)
         {
-            Pedido pedido1 = new Pedido("Mauricio", 150.0);
-            Pedido pedido2 = new Pedido("Marcelo", 250.0);
+            var cliente = new Cliente("Carlito", "Av. Jue, 42", DateTime.Now);
+            var cliente2 = PrototypeHelper.CreateDeepCopy(cliente);
+            cliente2.DataDeNascimento = cliente2.DataDeNascimento.AddMonths(5);
+            Console.WriteLine(cliente.Nome);
+            Console.WriteLine(cliente2.Nome);
+            Console.WriteLine(cliente.DataDeNascimento);
+            Console.WriteLine(cliente2.DataDeNascimento);
 
-            Workflow fila = new Workflow();
-
-            fila.Adiciona(new PagaPedido(pedido1));
-            fila.Adiciona(new PagaPedido(pedido2));
-            fila.Adiciona(new FinalizaPedido(pedido1));
-
-            fila.Processa();
+            Console.ReadKey();
         }
     }
 }
---
+++ Alura.Cursos.DesignPatterns/Prototype/PrototypeHelper.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.Json;
+
+namespace Alura.Cursos.DesignPatterns.Prototype
+{
+    public static class PrototypeHelper
+    {
+        public static T CreateDeepCopy<T>(T obj)
+        {
+            var str = JsonSerializer.Serialize(obj);
+            return JsonSerializer.Deserialize<T>(str);
+
+        }
+    }
+}
